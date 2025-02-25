94243d9e333b26ce03b505e48cb1a26ee9ad5ead
vitor <vitorfonsecaxs@gmail.com>
2025-02-25 11:07:56 -0300

    Add: criando a conexao MYSQL e o cursor e os dados do usuario

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import datetime
-import 
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -18,7 +18,36 @@ def post_mensagem():
 
     #cadastrando as informações do usuario no MYSQL (banco de dados)
 
+    #criando a conexao
+    conexao = mysql.connector.connect(
+        hostname = "localhost",
+        port = 3306,
+        user = "root",
+        password = "root",
+        database = "db_feedbacks")
+    
+    # criando o cursor ou seja o cara que vai do python ate o banco de dados como se fosse uma ponte
+    cursor = conexao.cursor()
+
+    # criando o comando SQL que sera executado no SQL
+    sql = """INSERT INTO tb_feedback
+        (name_user, comentario, data_comentario)
+    VALUES
+        (%s, %s, %s)"""
+    
+    # os %s sao os dados que serao informados no SQl como se fosse uma variavel entao usamos uma lista para guardar os dados 
+    valores = [nome_user, comentario, data_comentario]
+
+    # executando o comando SQL com o cursor
+    cursor.execute(sql, valores)
+
+    # confirmando a alteração no MYSQL como se fossse um comit do github
+    conexao.commit()
+
+    # fechando a conexao e o cursor
+    cursor.close()
+    conexao.close()
 
     return redirect("/")
 
-app.run(debug=True, host="0.0.0.0", port=8080)
+app.run(debug=True)
